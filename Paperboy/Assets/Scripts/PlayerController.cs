a88c841585d86d5eb021027df5836bff5d92fcb7
Sento920 <ruschta@uwec.edu>
2016-02-25 11:27:45 -0600

    Commit of base project. Currently have the road, and bicycle assets. Next step is getting the throwing mechanic working successfully complete with powerbar.

---
+++ Paperboy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+    float direct;
+    public float oomph;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        direct = Input.GetAxis("Vertical");
+	}
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(0,0,direct * oomph));
+    }
+
+}
